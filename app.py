5b6c1a84297c27abfe5c6de87e758887a8a4e703
mdaipayan <49827916+mdaipayan@users.noreply.github.com>
2026-04-03 11:13:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -124,10 +124,13 @@ with col1:
             [16,22,A_leg,E_steel], [17,22,A_leg,E_steel], # -Y Peak Legs
             [21,22,A_brace,E_steel], # Tie between peaks
 
-            # 7. HORIZONTAL PLAN BRACING (Crucial for 3D torsional stability)
+           # 7. HORIZONTAL PLAN BRACING (Crucial for 3D torsional stability)
             [5,7,A_brace,E_steel], [6,8,A_brace,E_steel],     # L1 Diagonals
             [9,11,A_brace,E_steel], [10,12,A_brace,E_steel],   # L2 Diagonals
-            [15,17,A_brace,E_steel], [16,18,A_brace,E_steel]   # L3 Diagonals
+            [15,17,A_brace,E_steel], [16,18,A_brace,E_steel],  # 
+            
+            # 8. TOP PEAK X-BRACING (Prevents Windshield-Wiper Mechanism)
+            [15,22,A_brace,E_steel], [16,21,A_brace,E_steel]
             
         ], columns=["Node_I", "Node_J", "Area(sq.m)", "E (N/sq.m)"])
         
